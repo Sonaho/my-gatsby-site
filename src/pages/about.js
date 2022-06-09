70b3480d4b10135ce87bd3e5f38814bc7f66f899
Alejo Almela <alejo.almela@gmail.com>
2022-06-09 11:24:27 +0200

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <title>About me</title>
+            <h1>About me</h1>
+            <p>Hi theer! I'm the proud creator of this site, which i built with Gatsby.</p>
+        </main>
+    )
+}
+
+export default AboutPage
